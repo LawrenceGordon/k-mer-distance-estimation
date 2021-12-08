55d23e897132cbb8e9d6ab6d8d8b0866e7f48a69
Lawrence Gordon <lmg1081@ron.sr.unh.edu>
2021-12-07 20:13:33 -0500

    stable build pre-submission

--- download_fna.sh
+++ download_fna.sh
@@ -1,11 +1,22 @@
 #! /bin/bash
 
+# downloads latest refseq assembly summary
+wget https://ftp.ncbi.nlm.nih.gov/genomes/ASSEMBLY_REPORTS/assembly_summary_refseq.txt -O assembly_summary_refseq.txt
+
+# Uses $1 and $2 are separate parsing variables for organism names
 # gets link from refseq db
-link=$(cat $1 | grep $2 | grep $3 | head -n 1 | cut -f 20)
+link=$(grep $1 assembly_summary_refseq.txt | grep $2 | head -n 1 | cut -f 20)
+
+# exits program if no matching organism is found
+if [[ $link == '' ]]
+then
+    echo No matches found for $1 $2
+    exit 1
+fi
 
 # generates G_species dir
-g=$(expr substr $(echo $2) 1 1)
-new_dir=$(echo $g)_$3
+g=$(expr substr $(echo $1) 1 1)
+new_dir=$(echo $g)_$2
 mkdir $new_dir
 
 # gets file name
--- kmer_distance_estimation.py
+++ kmer_distance_estimation.py
@@ -10,8 +10,9 @@ def parse_args():
     parser.add_argument("fasta", nargs="+", help="fasta files containing nucleic acid sequences")
     parser.add_argument("-k", type=int, default=1, help="k-mer size")
     parser.add_argument("-m", action="store_true", 
-        help="if specified, returns mahanalobis distance; defaults to euclidean distance")
-    parser.add_argument("-n", action="store_true", help="if specified, normalizes distance to percent")
+        help="if specified, returns mahalanobis distance; defaults to euclidean distance")
+    parser.add_argument("-n", action="store_true", 
+        help="if specified, normalizes distance to percentage, and generates heatmap")
     parser.add_argument("--out", "-o", help="kmer data table output")
     return parser.parse_args()
 
@@ -35,8 +36,8 @@ def parse_fasta(fasta, k):
             kmer_dict.setdefault(kmer, 0)
             kmer_dict[kmer] += 1
             
-    print(fasta + ":" + organism)
-    return (organism, kmer_dict)    
+    print("Parsed {0} -------> Exracted Organism: {1}".format(fasta, organism))
+    return (organism, kmer_dict)
 
 # compares the keys in two dictionaries, and adds the missing keys
 # from each into one another
@@ -55,7 +56,7 @@ def euclid_kmer(dict_1, dict_2):
         euclid_sum += (dict_2[coord] - dict_1[coord])**2
     return math.sqrt(euclid_sum)
 
-# computes mahanalobis distance based on two kmer dictionaries
+# computes mahalanobis distance based on two kmer dictionaries
 def maha_kmer(dict_1, dict_2):
     maha_sum = 0
     for coord in dict_2:
@@ -103,85 +104,56 @@ def matrix_output(samples, k, maha):
 
     # distance matrix output; list of lists
     distances = []
-    for sample1 in samples:
+    for sample_1 in samples:
         sub_dist = []
-        for sample2 in samples:
+        for sample_2 in samples:
             # if distance already computed, grab cached value
-            if (sample2, sample1) in store_compare:
-                sub_dist.append(store_compare[sample2, sample1])
+            if (sample_2, sample_1) in store_compare:
+                sub_dist.append(store_compare[sample_2, sample_1])
                 continue
             # if samples are the same, dist = 0
-            if sample1 == sample2:
-                sub_dist.append(0.0)
+            if sample_1 == sample_2:
+                sub_dist.append(0.00)
                 continue
 
             # grabs kmer dictionaries from distance_dict
-            kmer1 = distance_dict[sample1]
-            kmer2 = distance_dict[sample2]
+            kmer_1 = distance_dict[sample_1]
+            kmer_2 = distance_dict[sample_2]
 
-            compare_kmer(kmer1, kmer2)
+            compare_kmer(kmer_1, kmer_2)
             # calculates distance using eithe mahalanobis distance or euclidean
             # distance based on command-line input
-            dist = maha_kmer(kmer1, kmer2) if maha else euclid_kmer(kmer1, kmer2)
+            dist = maha_kmer(kmer_1, kmer_2) if maha else euclid_kmer(kmer_1, kmer_2)
             # adds dist to cache dictionary
-            store_compare[(sample1, sample2)] = dist
+            store_compare[(sample_1, sample_2)] = dist
             sub_dist.append(dist)
 
         distances.append(sub_dist)
     return orgs, distances
 
-def make_table(samples, k, maha, norm, output):
-    ### {sample_N: {sample_N kmer dict}, ...} ###
-    distance_dict = {}
-    orgs = []
-
-    # pairs kmer dictionaries with organism names
-    for sample in samples:
-        org, distance_dict[sample] = parse_fasta(sample, k)
-        # adds organism to list for output header
-        if org not in orgs:
-                orgs.append(org)
-
-    ### {(sample_x, sample_y): dist, ...} ###
-    # stores previously computed distance values so program only
-    # has to run unique pairs of orgs
-    store_compare = {}
-
-    # distance matrix output; list of lists
-    distances = []
-    for sample1 in samples:
-        sub_dist = []
-        for sample2 in samples:
-            # if distance already computed, grab cached value
-            if (sample2, sample1) in store_compare:
-                sub_dist.append(store_compare[sample2, sample1])
-                continue
-            # if samples are the same, dist = 0
-            if sample1 == sample2:
-                sub_dist.append(0.0)
-                continue
-
-            # grabs kmer dictionaries from distance_dict
-            kmer1 = distance_dict[sample1]
-            kmer2 = distance_dict[sample2]
-
-            compare_kmer(kmer1, kmer2)
-            # calculates distance using eithe mahalanobis distance or euclidean
-            # distance based on command-line input
-            dist = maha_kmer(kmer1, kmer2) if maha else euclid_kmer(kmer1, kmer2)
-            # adds dist to cache dictionary
-            store_compare[(sample1, sample2)] = dist
-            sub_dist.append(dist)
-
-        distances.append(sub_dist)
-
-    print(distances)
-
+def make_table(k, norm, orgs, distances, output):
     # normalizes the data in distances if user specifies
     if norm:
         distances = normalize(distances)
 
-    print(distances)
+    # if data is not normalized, generates a tab-separated output file
+    else:
+        out_data = ("{0}-mer comparison\t{1}\n".format(k, ("\t").join(orgs)))
+        for idx, org in enumerate(orgs):
+            out_data += ("{0}\t{1}\n".format(org, ("\t").join(str(dist) for dist in distances[idx])))
+        # writes data to output file if specified
+        if output is not None:
+            try:
+                with open(output + ".txt", "w") as out_file:
+                    out_file.write(out_data)
+                    print("Distance matrix saved to {0}.txt".format(output))
+            except IOError:
+                print("Error writing to file")
+            return
+        # prints to standard out otherwise
+        else:
+            print(out_data)
+            return
 
     # generates a heatmap based on orgaism similarity
     fig, ax = plt.subplots()
@@ -189,35 +161,34 @@ def make_table(samples, k, maha, norm, output):
     ax.set_xticks(range(len(orgs))); ax.set_yticks(range(len(orgs)))
     ax.set_xticklabels(orgs); ax.set_yticklabels(orgs)
     ax.set_title("{0}-mer Distance Estimation".format(k))
-    #ax.minorticks_on
-    #ax.grid(True, which='minor', color='w', linestyle='-', linewidth=3)
 
     # adds value from distance matrix to correct square
-    for org1 in range(len(orgs)):
-        for org2 in range(len(orgs)):
-            data = ax.text(org1, org2, str(round(distances[org1][org2], 2)),
-                ha="center", va="center", color="w", fontsize=16)
-
-    #ax.invert_yaxis()
-    ax.xaxis.tick_top()
+    # only used on small datasets to prevent formatting errors
+    if len(orgs) <= 10:
+        for org_1 in range(len(orgs)):
+            for org_2 in range(len(orgs)):
+                data = ax.text(org_1, org_2, str(round(distances[org_1][org_2], 2)),
+                    ha="center", va="center", color="w", fontsize=16)
 
     # creates heatmap from axes and matrix
+    ax.xaxis.tick_top()
     heatmap = ax.imshow(distances, cmap="coolwarm")
     fig.colorbar(heatmap)
 
     plt.setp(ax.get_xticklabels(), rotation=45, ha="left", 
         va="top", rotation_mode="anchor", fontsize=10)
 
-    #for tick in ax.xaxis.get_majorticklabels():
-    #tick.set_horizontalalignment("left")
     plt.setp(ax.get_yticklabels(), fontsize=10)
-    #plt.setp(ax.get_xticklabels(), fontsize=10)
-    #plt.tight_layout()
-    #plt.show()
-    #fig.savefig(output, format="jpg")
 
     fig.set_size_inches(10, 8)
     plt.savefig(output + ".jpg", bbox_inches="tight")
+    print("Heatmap saved to {0}.jpg".format(output))
 
 args = parse_args()
-make_table(args.fasta, args.k, args.m, args.n, args.out)
+
+# Ensures enough arguments given for comparison
+if len(args.fasta) > 1:
+    orgs, distances = matrix_output(args.fasta, args.k, args.m)
+    make_table(args.k, args.n, orgs, distances, args.out)
+else:
+    print("Must specify at least two fasta files for comparison")
